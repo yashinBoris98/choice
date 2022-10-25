ff3d4d95d9807d21dd0b0c5649ac9aa58ce09782
yashinBoris98 <111410571+yashinBoris98@users.noreply.github.com>
2022-10-25 09:39:42 +0300

    программма

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("enter array lenghth");
+int arrLength = Convert.ToInt32(Console.ReadLine());
+String[] arr = new string[arrLength];
+
+String? a="";
+for (int i = 0; i < arrLength; i++)
+{
+    a=Console.ReadLine();
+    if(a!=null)
+    arr[i]=a;
+}
+
+String[] arr1 = new string[arrLength];
+for (int i = 0; i < arrLength; i++)
+{
+    int num = new Random().Next(1,3);
+    if(num>arr[i].Length)
+        arr1[i]=arr[i];
+    else
+        arr1[i]=arr[i].Substring(0,num);
+
+}
+
+foreach (var item in arr1)
+{
+    Console.WriteLine(item);
+}
+
+
